976dfca12969f166e4b4c89b895e40c2febdbaea
swarupsarkardeveloper <swarupsarkardeveloper@gmail.com>
2026-04-15 09:04:14 +0530

    Add LinkedIn link to sidebar and mobile dashboard dev credit

--- js/app.js
+++ js/app.js
@@ -332,6 +332,13 @@ const App = (() => {
 
       <div class="section">
         ${buildCalendar(plan, todayStr)}
+      </div>
+
+      <div class="mobile-dev-credit">
+        <a href="https://www.linkedin.com/in/swaruupssarkar/" target="_blank" rel="noopener">
+          <svg viewBox="0 0 24 24" fill="currentColor" width="14" height="14"><path d="M20.45 20.45h-3.55v-5.57c0-1.33-.03-3.04-1.85-3.04-1.85 0-2.13 1.45-2.13 2.94v5.67H9.37V9h3.41v1.56h.05c.48-.9 1.64-1.85 3.37-1.85 3.6 0 4.27 2.37 4.27 5.45v6.29zM5.34 7.43a2.06 2.06 0 1 1 0-4.12 2.06 2.06 0 0 1 0 4.12zM7.12 20.45H3.56V9h3.56v11.45zM22.22 0H1.77C.79 0 0 .77 0 1.72v20.56C0 23.23.79 24 1.77 24h20.45C23.2 24 24 23.23 24 22.28V1.72C24 .77 23.2 0 22.22 0z"/></svg>
+          Developed by Swaruup
+        </a>
       </div>`;
 
     bindCalendarEvents();
